6c6bf66a96eed0d4959482cbb902a9835793b2b9
Guangxu Li <guangxu.li@nyu.edu>
2023-01-16 17:24:28 +0800

    chore: refactor clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-mv 1??.*.py ./101-200 > /dev/null 2>&1 || true
-mv 2??.*.py ./201-300 > /dev/null 2>&1 || true
-mv 3??.*.py ./301-400 > /dev/null 2>&1 || true
-mv 4??.*.py ./401-500 > /dev/null 2>&1 || true
-mv 5??.*.py ./501-600 > /dev/null 2>&1 || true
-mv 6??.*.py ./601-700 > /dev/null 2>&1 || true
-mv 7??.*.py ./701-800 > /dev/null 2>&1 || true
-mv 8??.*.py ./801-900 > /dev/null 2>&1 || true
-mv 9??.*.py ./901-1000 > /dev/null 2>&1 || true
-mv 10??.*.py ./1001-1100 > /dev/null 2>&1 || true
-mv 11??.*.py ./1101-1200 > /dev/null 2>&1 || true
-mv 12??.*.py ./1201-1300 > /dev/null 2>&1 || true
-mv 13??.*.py ./1301-1400 > /dev/null 2>&1 || true
-mv 14??.*.py ./1401-1500 > /dev/null 2>&1 || true
-mv 15??.*.py ./1501-1600 > /dev/null 2>&1 || true
-mv 16??.*.py ./1601-1700 > /dev/null 2>&1 || true
-mv 17??.*.py ./1701-1800 > /dev/null 2>&1 || true
-mv 18??.*.py ./1801-1900 > /dev/null 2>&1 || true
-mv 19??.*.py ./1901-2000 > /dev/null 2>&1 || true
-mv 20??.*.py ./2001-2100 > /dev/null 2>&1 || true
-mv 21??.*.py ./2101-2200 > /dev/null 2>&1 || true
-mv 22??.*.py ./2201-2300 > /dev/null 2>&1 || true
-mv 23??.*.py ./2301-2400 > /dev/null 2>&1 || true
-mv 24??.*.py ./2401-2500 > /dev/null 2>&1 || true
-mv 25??.*.py ./2501-2600 > /dev/null 2>&1 || true
-mv *.py ./1-100 > /dev/null 2>&1 || true
+for i in {1..25}; do
+    let j=$i+1
+    files="${i}??.*.py"
+
+    lo="${i}01"
+    hi="${j}00"
+    folder="./${lo}-${hi}"
+
+    mv ${files} ${folder} >/dev/null 2>&1 || true
+done
+
+mv *.py ./1-100 >/dev/null 2>&1 || true
